6bd0ce56bee4033f53de287457d18d5f43652117
JoeDiaz <joediazq93@gmail.com>
2022-12-14 18:02:14 -0800

    adding dist module and scripting for deployment

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/*jquery method click code*/
+$(function() {
+    $(".carousel").carousel({interval: 2000});
+    $("#carouselPause").click(function(){
+        if ($("#carouselPause").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselPause").children("i").removeClass("fa-pause");
+            $("#carouselPause").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselPause").children("i").removeClass("fa-play");
+            $("#carouselPause").children("i").addClass("fa-pause");
+        }
+    });
+});
